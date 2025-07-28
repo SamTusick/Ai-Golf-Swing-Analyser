3204fba3c47b6b440bac241b4abe4e11409808f0
SamTusick <stusick@outlook.com>
2025-07-28 11:14:29 -0400

    Allowed for video detection and drawing landmarks

--- gather-landmarks.py
+++ gather-landmarks.py
@@ -5,13 +5,32 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+# Video Feed
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame =cap.read()
-    cv2.imshow('MediaPipe Feed', frame)
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+# Can bump up confidence leveles if needed in future
+with mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose:
+    while cap.isOpened():
+        ret, frame =cap.read()
+
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        image.flags.writeable = False
+
+        # Making detection
+        results = pose.process(image)
+
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv2.imshow('MediaPipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
