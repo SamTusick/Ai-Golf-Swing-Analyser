f51e9aa5f52e95108cc57111b838817c42f4933e
SamTusick <stusick@outlook.com>
2025-07-28 10:23:10 -0400

    Added model for mediapipe

---
+++ gather-landmarks.py
@@ -0,0 +1,6 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+model_path = "/Ai-Golf-Swing/Ai-Golf-Swing-Analyser/pose_landmarker_full.task"
+
